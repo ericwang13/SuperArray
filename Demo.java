99d5f9cc523030616a208017d5b06887119b051b
ericzw <ericwang13@protonmail.com>
2020-11-08 14:34:48 -0500

    Added removeDuplicates and findOverlap

--- Demo.java
+++ Demo.java
@@ -8,23 +8,14 @@ public class Demo {
         }
     }
 
-    public static void main(String[] args) {
-        SuperArray words = new SuperArray();
-        // grouped to save vertical space
-        words.add("kani");
-        words.add("uni");
-        words.add("ebi");
-        words.add("una");
-        words.add("una");
-        words.add("ebi");
-        words.add("kani");
-        words.add("una");
-        words.add("una");
-        words.add("ebi");
-        words.add("toro");
-
-        System.out.println(words);
-        removeDuplicates(words);
-        System.out.println(words);
+    public static SuperArray findOverlap(SuperArray a, SuperArray b) {
+        SuperArray overlap = new SuperArray();
+        for (int i = 0; i < a.size(); i++) {
+            if (b.contains(a.get(i))) {
+                overlap.add(a.get(i));
+            }
+        }
+        removeDuplicates(overlap);
+        return overlap;
     }
 }
